0e40dedf7082686279ec16875508ab66fcdee494
Azharangga Kusuma <97172969+azharanggakusuma@users.noreply.github.com>
2022-09-07 15:14:46 +0700

    Delete script.js

--- p03jquery/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $("td").click(function (event) {
-        alert("Anda mengklik kolom yang berisi kata " + $(this).html() + "!");
-    });
-    $("th").click(function (event) {
-        alert("Anda mengklik kolom yang berisi kata  " + $(this).html() + "!");
-    });
-});
