8fc7afe2622a36e1ffd90f8e48a733b668e12a66
Azharangga Kusuma <97172969+azharanggakusuma@users.noreply.github.com>
2022-10-01 12:16:06 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.navigasi').delegate('.tidak-aktif', 'click', function(event) {
+	    var $this = $(this);
+	    $this.removeClass('tidak-aktif').addClass("aktif");
+        $this.siblings().addClass('tidak-aktif').removeClass('aktif');
+    });
+});
+
+function konfirmasiKembali(){
+    var kembali = confirm("Apakah ingin kembali ke halaman utama?");
+
+    if(kembali){
+        window.location = "../index.html";
+    }else{
+        window.location = "tabel.html";
+    }
+}
+
+function konfirmasiKembali2(){
+    var kembali = confirm("Apakah ingin kembali ke halaman utama?");
+
+    if(kembali){
+        window.location = "../index.html";
+    }else{
+        window.location = "tabel-2.html";
+    }
+}
